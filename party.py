85a63fbb63af93cf09385892fa147c4aa1e0ce99
rosesho <pjs1054@usnh.edu>
2025-07-05 19:43:29 -0400

    intial gameplay file

---
+++ party.py
@@ -0,0 +1,14 @@
+import random
+class Player:
+    def __init__ (self, name):
+        #deck of cards, 2 of each type
+        self.cards = [1, 1, 0, 0, -1, -1]
+        #relationship level
+        self.status = 10
+        #character name
+        self.name = name
+
+you = Player("Lucy")
+stevie = Player("Stevie")
+marrow = Player("Marrow")
+thorn = Player("Thorn")
